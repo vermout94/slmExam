66e108b85c431be62a8272d8877ba332bb4d8ab0
vermout94 <david.berger4494@gmail.com>
2023-06-27 21:43:34 +0200

    Initial commit (without mvn packages)

---
+++ src/test/java/at/technikum/slmExam/SlmExamApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
