dde1275a029128e09875fe4a93b28b5d76360e63
vermout94 <david.berger4494@gmail.com>
2023-06-28 18:18:05 +0200

    added new feature to calculate total sum of factorial

--- src/main/java/at/technikum/slmExam/service/FactorialService.java
+++ src/main/java/at/technikum/slmExam/service/FactorialService.java
@@ -5,14 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class FactorialService {
 
+    private long totalSum = 0;
+
     public long calculateFactorial(int number) {
         if (number < 0) {
             throw new IllegalArgumentException("Input number should be a non-negative integer.");
         }
 
         long result = factorial(number);
+        totalSum += result;
         return result;
     }
+
+    public long getTotalFactorialSum() {
+        return totalSum;
+    }
+
     private long factorial(int number) {
         long result = 1;
 
